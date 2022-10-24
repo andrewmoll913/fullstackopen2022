2bb2ef0d7ea4a527bf0d9e66f929700ff1093db9
Andrew Moll <andrewmoll@ymail.com>
2022-10-24 17:14:43 +1300

    Perform exercise 1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,18 +1,23 @@
 import { useState } from 'react'
 
-const Header = ({text}) => {
+const Header = ({ text }) => {
   return (
     <h1>{text}</h1>
   )
 }
 
-const Button = ({handleClick, text}) => {
+const Button = ({ handleClick, text }) => {
   return (
     <button onClick={handleClick}>{text}</button>
   )
 }
 
-const Rating =({text, amount}) => <p>{text} {amount}</p>
+const Data = ({ text, amount }) => <p>{text} {amount}</p>
+
+const calculateAverage = (all, good, bad) => all === 0 ? 0 : (good - bad) / all
+
+
+const calculatePostive = (all, good) => all === 0 ? "0 %" : `${(good / all) * 100} %`;
 
 const App = () => {
   // save clicks of each button to its own state
@@ -20,9 +25,13 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleGoodClicks = () => setGood(good + 1);
-  const handleNeutralClicks = () => setNeutral(neutral + 1);
-  const handleBadClicks = () => setBad(bad + 1);
+  const ratingHander = (rating, setRating) => () => {
+    setRating(rating + 1) 
+  }
+
+  const handleGoodClicks = ratingHander(good, setGood)
+  const handleNeutralClicks = ratingHander(neutral, setNeutral)
+  const handleBadClicks = ratingHander(bad, setBad);
 
   return (
     <div>
@@ -31,9 +40,12 @@ const App = () => {
       <Button text="neutral" handleClick={handleNeutralClicks} />
       <Button text="bad" handleClick={handleBadClicks} />
       <Header text="statistics" />
-      <Rating text="good" amount={good} />
-      <Rating text="neutral" amount={neutral} />
-      <Rating text="bad" amount={bad} />
+      <Data text="good" amount={good} />
+      <Data text="neutral" amount={neutral} />
+      <Data text="bad" amount={bad} />
+      <Data text="all" amount={good + neutral + bad} />
+      <Data text="average" amount={calculateAverage(good + neutral + bad, good, bad)} />
+      <Data text="positive" amount={calculatePostive(good + neutral + bad, good)} />
     </div>
   )
 }
